d7a249ba56c28c2066f84c18973e509a8df28b5a
Abanoub Ragaay <abanoub.ragaay@CLPDC.LOCAL>
2024-07-22 12:36:55 +0300

    add

--- app.js
+++ app.js
@@ -1,112 +1,179 @@
-
-// Encapsulation examples
-// JS native
-// function Person(fname,lname){
-//   let firstname = fname;
-//   let lastname = lname;
-
-//   let getDetails_noaccess = function(){
-//       return (`My name is: ${firstname} ${lastname}`);
+//? Classes in ES6
+// class Person {
+//   name;
+//   height;
+//   weight;
+//   nationality = "Egyptian";
+//   constructor(name, h, w) {
+//     this.name = name;
+//     this.height = h;
+//     this.weight = w;
 //   }
-
-//   this.getDetails_access = function(){
-//     console.log('trying to access the private method within the class =>',getDetails_noaccess())
-//       return (`Hi, I am ${firstname} ${lastname}`);
-//   }
-// }
-// let person1 = new Person('Mukul','Latiyan');
-// console.log('trying to access the private prop from the instance =>',person1.firstname); // private prop can't be accessed
-// console.log('trying to access the private method from the instance =>',person1.getDetails_noaccess); 
-// console.log('trying to access the public method from the instance =>',person1.getDetails_access());
-    
-
-
-// ES6 
-
-// class Person{
-//   constructor(x,speed){
-//     let name = x;
-//     this.speed = speed;
-//     this.move = function () {
-//       // can access both name and speed from here
-//       console.log(`${name} moves with the speed of ${this.speed} miles per second!`);
-//     }
+//   greet() {
+//     console.log(`Hello this is ${this.name} I am ${this.nationality}`);
 //   }
 //   jump() {
-//     // cannot access speed from here
-//     console.log(`${name} jumps on top of the building!`);
-//     console.log(`${this.speed} is his speed`);
+//     console.log("I can Jumpt");
 //   }
 // }
-// let person1 = new Person("Aba",500);
-// console.log('trying to access the private prop from the instance =>',person1.name);
-// console.log('trying to access the public prop from the instance =>',person1.speed);
-// person1.move()
-// person1.jump()
+
+//? Static Props and Methods
+
+// class Person {
+//   name;
+//   height;
+//   weight;
+//   nationality = "Egyptian";
+//   static count = 0;
+//   constructor(name, h, w) {
+//     this.name = name;
+//     this.height = h;
+//     this.weight = w;
+//     Person.count++;
+//   }
+//   greet() {
+//     console.log(`Hello this is ${this.name} I am ${this.nationality}`);
+//   }
+//   jump() {
+//     console.log("I can Jumpt");
+//   }
+
+//   static definePerson() {
+//     console.log("a person has a name, height and weight");
+//   }
+//   static setCount(v) {
+//     Person.count = v;
+//   }
+// }
+
+//? Getter & Setter Methods
+
+// class Person {
+//   #name;
+//   height;
+//   weight;
+//   nationality = "Egyptian";
+//   static count = 0;
+//   constructor(name, h, w) {
+//     this.#name = name;
+//     this.height = h;
+//     this.weight = w;
+//     Person.count++;
+//   }
+//   greet() {
+//     console.log(`Hello this is ${this.#name} I am ${this.nationality}`);
+//   }
+//   jump() {
+//     console.log("I can Jumpt");
+//   }
+
+//   static definePerson() {
+//     console.log("a person has a name, height and weight");
+//   }
+//   static setCount(v) {
+//     Person.count = v;
+//   }
+
+//   getName() {
+//     return this.#name;
+//   }
+//   get name() {
+//     return this.#name;
+//   }
+
+//   set setname(v){
+//     this.#name = v;
+//   }
+// }
+// const per = new Person("ahmed",175,70)
 
 
-// Inheritance in ES6
+
+//? Object Oriented Programming Concepts
+
+//? Encapsulation in  ES6
+
+// class User{
+//     #name
+
+//     constructor(name, password){
+//         this.#name = name
+//         this._password = password
+//     }
+    
+//     #printName(){
+//         console.log(this.#name);
+//     }
+
+//     PrintFromPrivateMethod(){
+//         this.#printName()
+//     }
+// }
+
+// const kedar = new User("kedar", 123456)
+// kedar.#name
+
+//? Inheritance 
+//* ES6
+
+// class Person {
+//     name;
+//     id;
+//     constructor(name , id){
+//         this.name=name;
+//         this.id=id;
+//     }
+// }
+// class Teacher extends Person{
+//     payment;
+//     constructor(name,id,payment){
+//         super(name,id)
+//         this.payment = payment;
+//     }
+// }
+// let t1 = new Teacher("abanoub","dsadasdasdsa",5000);
+
+//! JS
+
+// function Person(name,id){
+//     this.name = name;
+//     this.id=id;
+// }
+// function Teacher(name,id, payment){
+//     this.payment = payment;
+    
+// }
+// Teacher.prototype = new Person()
+
+// let t1 = new Teacher(5000);
+// t1.__proto__.name = "abanoub";
+// t1.__proto__.id = "dsadasdasdsa";
+
+
+// console.log(t1);
+
+
+//? Methods and Props Overriding in ES6
 
 // class Person {
 //     name;
 //     constructor(name){
 //         this.name=name;
+//         this.occupation = "unemployed"
 //     }
+//     greet(){console.log("hello I am a "+this.occupation)}
+//     introduce(){console.log("hello I am "+this.name)}
 
 // }
 // class Teacher extends Person{
-//     payment;
+//     payment;occupation;
 //     constructor(name , payment){
 //         super(name)
 //         this.payment = payment;
+//         this.occupation = "Teacher"
 //     }
+//     greet(){console.log(this.occupation+" inheritance and ovverrriding")}
+//     static test(){console.log("I am a Static method", this);}
 // }
-// let t1 = new Teacher("abanoub",5000);
-
-// Methods and Props Overriding in Es6
-
-class Person {
-    name;
-    constructor(name){
-        this.name=name;
-        this.occupation = "unemployed"
-    }
-    greet(){console.log("hello I am a "+this.occupation)}
-    introduce(){console.log("hello I am a "+this.name)}
-
-}
-class Teacher extends Person{
-    payment;occupation;
-    constructor(name , payment){
-        super(name)
-        this.payment = payment;
-        this.occupation = "Teacher"
-    }
-    greet(){console.log(this.occupation+" inheritance and ovverrriding")}
-    static test(){console.log("I am a Static method", this);}
-}
-let t1 = new Teacher("abanoub",5000);
-console.log();
-Teacher.test()
-
-// // Inheritance in JS
-// function Person(name){
-//     this.name = name;
-//     this.occupation = "unemployeed"
-//     this.greet=function (x){
-//         console.log(x,"person constructor ");
-//     }
-// }
-// function Student(name, grade){
-//     this.grade = grade;
-//     this.greet=function (x,y){
-//         console.log(x,"student constructor greets "+y);
-//     }
-// }
-// Student.prototype = new Person()
-// let s2 = new Student("bodda", "5th")
-// let s3 = new Student("bodda", "5th")
-// let s4 = new Student("bodda", "5th")
-
-// s3.greet("abanoub")
-
+// let t1 = new Teacher("Ahmed",5000);
+// console.log(t1);
